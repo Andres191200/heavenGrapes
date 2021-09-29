684a764b99776d96a580edb8e29fb07a7e7f3796
Andrew <marianoandres191200@gmail.com>
2021-09-29 03:23:01 -0300

    main section added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import ReactDOM from 'react-dom';
+
 import Header from './components/header.jsx';
+import Welcome from './components/welcome.jsx'
 
 
 function App() {
   return (
-    <div className="header-container">
-      <Header />
-    </div>
+    <Fragment>
+      <div className="header-container">
+        <Header />
+      </div>
+
+      <Welcome />
+    </Fragment>
   );
 }
 
---
+++ src/components/welcome.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../styles/welcomeText.css';
+
+const Welcome = () =>{
+	return(
+		<div className="welcome-container">
+			<div className="welcome-text">
+				<h1>HEAVEN</h1><h2>GRAPES</h2>
+			</div>
+		</div>
+	);
+}
+
+export default Welcome;
