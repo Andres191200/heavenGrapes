9ac9098b98737bda67334dbbace158906b7e028e
Andrew <marianoandres191200@gmail.com>
2021-10-18 02:48:21 -0300

    slider styles added

--- src/components/infoSection.jsx
+++ src/components/infoSection.jsx
@@ -8,25 +8,30 @@ import Slider from './slider';
 
 const infoSection = () =>{
 	return(
-		<div className="info-container">
-			<div className="img-container">
-				<ul>	
-					{
-						slider.map((item, index) =>
-							<Slider key={index} index={item.key}/>
-						)
-					}
-				</ul>
+			<div className="info-container">
+				<div className="curve">
+				</div>
+				<div className="img-container">
+					<ul>	
+						{
+							slider.map((item, index) =>
+								<Slider key={index} index={item.key}/>
+							)
+						}
+					</ul>
+				</div>
+				<div className="about-us-container">
+					<h1>OUR</h1><h1>COMPANY</h1>
+					<p>We want clients to agree that our wines are the best, that's why
+					we focus on quality and performance, if you wanna taste a good wine, you are in the correct 
+					place, every type of wine is here, sweets wines, acids, tastiers, and so on...
+					</p>
+					<h2>we hope you enjoy our wines :D</h2>
+				</div>
+				<img class="wine-bottle" src={require('../media/img/wine-bottle.png').default}/>
+
 			</div>
-			<div className="about-us-container">
-				<h1>OUR</h1><h1>COMPANY</h1>
-				<p>We want clients to agree that our wines are the best, that's why
-				we focus on quality and performance, if you wanna taste a good wine, you are in the correct 
-				place, every type of wine is here, sweets wines, acids, tastiers, and so on...
-				</p>
-				<h2>we hope you enjoy our wines :D</h2>
-			</div>
-		</div>
+
 		);
 }
 
