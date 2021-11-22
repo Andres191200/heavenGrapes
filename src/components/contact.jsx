5c56b10b38c34e051edaf675249120dd1ff7a634
Andrew <marianoandres191200@gmail.com>
2021-11-22 14:14:00 -0300

    test commit

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -2,9 +2,16 @@ import React, {Fragment, useState as UseState} from 'react';
 
 import '../styles/contact.css';
 
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+
+import {faUser, faTruck, faBuilding, faUsers} from '@fortawesome/free-solid-svg-icons';
+
 import Header from './header.jsx';
 import options from '../contact-view-options.json';
-import Option from './option.jsx';
+import Option from './option.jsx'; 	
+
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.familyPrefix = 'fa';
 
 const contact = props =>{
 
@@ -36,16 +43,24 @@ const contact = props =>{
 						</div>
 						<div className="info-container">				
 							<div className="option-info-1">
-								<h1>A</h1>
+								{
+									option!='' ? <FontAwesomeIcon icon={['fas',option.icons[0]]}/> : ''
+								}
 							</div>
 							<div className="option-info-2">
-								<h1>B</h1>
+								{
+									option!='' ? <FontAwesomeIcon icon={faBuilding}/> : ''
+								}
 							</div>
 							<div className="option-info-3">
-								<h1>C</h1>
+								{
+									option!='' ? <FontAwesomeIcon icon={option.icons[2]}/> : ''
+								}
 							</div>
 							<div className="option-info-4">
-								<h1>D</h1>
+								{
+									option!='' ? <FontAwesomeIcon icon={option.icons[3]}/> : ''
+								}
 							</div>
 						</div>
 					</div>
